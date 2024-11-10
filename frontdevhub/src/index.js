edd558e3e0e049420eeaf9bae481bb4a67e3b322
Edson Correia <edsoncorreia997@gmail.com>
2024-11-10 18:57:11 -0300

    pequenos ajustes nas fontes

--- frontdevhub/src/index.js
+++ frontdevhub/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Home from './pages/home';
+import AdicionarColecao from './pages/adicionarColecao';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
