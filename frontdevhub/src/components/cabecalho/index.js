179d9aa0ed94a27895db38754e890f849cebaf1b
Joaquin <julcamoro1998@hotmail.com>
2024-11-14 02:47:32 -0300

    OK

--- frontdevhub/src/components/cabecalho/index.js
+++ frontdevhub/src/components/cabecalho/index.js
@@ -12,35 +12,28 @@ export default function Cabecalho({ onLoginClick, showLogin }) {
     return (
         <header className='cabecalho'>
             <div className='cabecalho-logo'>
+                <h1>DEV<p>HUB</p></h1>
                 {localStorage.getItem('TOKEN') ? (
-                    <div  >
-                        <h1 >DEV<p>HUB</p></h1>
-                        <span className="logoff-link">
-                            Retornar para &nbsp;
-                            <a href="#" onClick={logOff} className="logoff-button">
-                                portada
-                            </a>
-                        </span>
-                    </div>
+
+                    <span className="logoff-link">
+                        Retornar para &nbsp;
+                        <a href="#" onClick={logOff} className="logoff-button">
+                            portada
+                        </a>
+                    </span>
                 ) : (
-                    <div className="cabecalho">
-                        <div className="cabecalho-logo" style={{
-                            width: 750 + 'px'
-                        }}>
-                            <h1>DEV<p>HUB</p></h1>
-                        </div>
-                        <div className='nav'>
+                    <>
+                        <div className='nav' >
                             <a href='#home' className='link'>Home</a>
                             <a href='#portfolio' className='link'>Portfólio</a>
                             <a href='#agenda' className='link'>Agenda</a>
                             <a href='#mensagens' className='link'>Mensagens</a>
                             <a href='#faturamento' className='link'>Faturamento</a>
                         </div>
-
                         <button onClick={onLoginClick} className="cabecalho-botao">
                             <b>Login</b>
                         </button>
-                    </div>
+                    </>
                 )}
             </div>
         </header>
